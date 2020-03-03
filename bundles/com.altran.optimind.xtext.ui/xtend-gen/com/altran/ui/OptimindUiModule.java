08580c664343409be1887fed4870edae01c1567e
Mamadou Mendy <mamadou.mendy@mathinfo.i-univ-tlse2.fr>
2020-03-03 10:48:59 +0100

    Consolidate optimind projects

--- bundles/com.altran.optimind.xtext.ui/xtend-gen/com/altran/ui/OptimindUiModule.java
+++ bundles/com.altran.optimind.xtext.ui/xtend-gen/com/altran/ui/OptimindUiModule.java
@@ -13,7 +13,7 @@ import org.eclipse.xtend.lib.annotations.FinalFieldsConstructor;
 @FinalFieldsConstructor
 @SuppressWarnings("all")
 public class OptimindUiModule extends AbstractOptimindUiModule {
-  public OptimindUiModule(final AbstractUIPlugin arg0) {
-    super(arg0);
+  public OptimindUiModule(final AbstractUIPlugin plugin) {
+    super(plugin);
   }
 }
